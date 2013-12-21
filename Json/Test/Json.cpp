54129f8b06d07deee527fe4ab391d795fdf2d7f8
hanhuiLinux <zhujingfa@gmail.com>
2013-12-21 19:02:58 +0800

    Json正反方向编码

--- Json/Test/Json.cpp
+++ Json/Test/Json.cpp
@@ -6,9 +6,14 @@ using namespace Json;
 
 int main(int argc, char **argv){
     void testEncode();
-	for(int iter=0; iter<=1000000; iter++){
-		testEncode();
+	for(int iter=0; iter<1; iter++){
+	    testEncode();
 	}
+
+	void testDecode();
+    for(int iter=0; iter<1; iter++){
+        testDecode();
+    }
 	return 0;
 }
 
@@ -18,11 +23,21 @@ void testEncode(){
 	string jsonString="{\"name\":\"zhujingfa\",\"age\":26,\"ename\":\"hanhui\"}";
 
 	if(reader->parse(jsonString, value)){
+	    //如果注释，不涉及IO，则快好多。
 		cout<<"Name:"<<value["name"].asCString()<<"\n";
 		cout<<"Age:"<<value["age"].asInt()<<"\n";
 		cout<<"Ename:"<<value["ename"].asCString()<<endl;
 	}
 
 	delete reader;
+}
+
+void testDecode(){
+    Value value;
+    value["name"]="zhujingfa";
+    value["age"]=26;
+    value["ename"]="hanhui";
+
+    cout<<value.toStyledString()<<endl;
 }
 
