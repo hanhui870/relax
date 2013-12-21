533e1802c3896a1dd74f496362666fd0c5d80f6d
hanhuiLinux <zhujingfa@gmail.com>
2013-12-21 18:53:29 +0800

    Json内存泄漏问题 benchmark测试

--- Json/Test/Json.cpp
+++ Json/Test/Json.cpp
@@ -5,6 +5,14 @@ using namespace std;
 using namespace Json;
 
 int main(int argc, char **argv){
+    void testEncode();
+	for(int iter=0; iter<=1000000; iter++){
+		testEncode();
+	}
+	return 0;
+}
+
+void testEncode(){
 	Reader *reader=new Reader();
 	Value value;
 	string jsonString="{\"name\":\"zhujingfa\",\"age\":26,\"ename\":\"hanhui\"}";
@@ -15,5 +23,6 @@ int main(int argc, char **argv){
 		cout<<"Ename:"<<value["ename"].asCString()<<endl;
 	}
 
-	return 0;
+	delete reader;
 }
+
