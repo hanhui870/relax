c2320c21d625c8ba3d57f2d5081f2d55e17b5ed7
hanhuiLinux <zhujingfa@gmail.com>
2013-12-22 12:44:53 +0800

    Mysqlpp测试 libmysqlpp已经链接过libmysql

--- Mysql/Test/MysqlPP.cpp
+++ Mysql/Test/MysqlPP.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
-
+#include <mysql++/mysql++.h>
 
 using namespace std;
-using namespace Json;
+using namespace mysqlpp;
 
 int main(int argc, char **argv){
+    try {
+        Connection conn(true);
+        if(!conn.connect("iyunlin", "localhost", "hanhui", "han@#$%2213")){
+            cout<<"DB connect error."<<endl;
+            return 0;
+        }
 
+        Query query = conn.query("SELECT * FROM  `system_log` LIMIT 0 , 30");
+        StoreQueryResult res = query.store();
+
+        StoreQueryResult::const_iterator iter;
+        //前置自增性能更好
+        for(iter=res.begin(); iter!=res.end(); ++iter){
+            Row it=*iter;
+            cout<<it[0]<<"\t"<<it[1]<<"\t"<<it[2]<<"\t"<<it[3]<<"\n";
+        }
+
+    }catch (const BadQuery& er) {
+        // Handle any query errors
+        cerr << "Query error: " << er.what() << endl;
+        return -1;
+    }
+
+    return 0;
 }
 
 
