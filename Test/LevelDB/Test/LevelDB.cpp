88da597ed16af5f303803dc9fc2f694ed9769620
Bruce <bruce@duomai.com>
2014-01-24 11:28:44 +0800

    批量写入测试

--- Test/LevelDB/Test/LevelDB.cpp
+++ Test/LevelDB/Test/LevelDB.cpp
@@ -7,14 +7,20 @@ int main(int argc, char **argv){
 	leveldb::Options options;
 	options.create_if_missing=true;
 
-	//指定的testdb是个目录
+	//鎸囧畾鐨刣b.test鏄釜鐩綍
 	leveldb::Status status = leveldb::DB::Open(options, "db.test", &db);
 	if(!status.ok()){
-		cerr<< status.ToString()<<endl;
+		std::cerr<< status.ToString()<<std::endl;
 		return 1;
 	}
 
-	db->Put(leveldb::WriteOptions(), "hanhui", "A good man!");
+	std::string name("hanhui");
+	std::string key;
+	for(int iter=0; iter<1000000; iter++){
+		key.append(name+static_cast<std::string>(iter));
+		db->Put(leveldb::WriteOptions(), name, "A good man!");
+	}
+
 	delete db;
 	return 0;
 }
--- Test/LevelDB/Test/get.cpp
+++ Test/LevelDB/Test/get.cpp
@@ -1,23 +1,21 @@
 #include <iostream>
 #include <leveldb/db.h>
-using namespace std;
 
 int main(int argc, char **argv){
 	leveldb::DB *db=NULL;
 	leveldb::Options options;
 	options.create_if_missing=true;
 
-	//指定的testdb是个目录
 	leveldb::Status status = leveldb::DB::Open(options, "db.test", &db);
 	if(!status.ok()){
-		cerr<< status.ToString()<<endl;
+		std::cerr<< status.ToString()<<std::endl;
 		return 1;
 	}
 
 	std::string value;
 	status =db->Get(leveldb::ReadOptions(), "hanhui", &value);
 	if(status.ok()){
-		cout<<"LevelDb hanhui's Value:"<<value<<endl;
+		std::cout<<"LevelDb hanhui's Value:"<<value<<std::endl;
 	}
 	delete db;
 	return 0;
