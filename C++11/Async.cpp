53bfd7c7d0d93e2d70e35efe8f40022a6dcf4a95
HanHui <zhujingfa@gmail.com>
2014-08-10 21:51:31 +0800

    线程异步调用 最终实现了；指定了必须异步。

--- C++11/Async.cpp
+++ C++11/Async.cpp
@@ -3,34 +3,39 @@
 #include <thread>
 #include <future>
 #include <chrono>
+#include <string>
+#include <mutex>
 
 using namespace std;
 
-void echo1(){
-	int i=0;
-	while(i<10){
-		cout<<"echo1"<<endl;
+//使用了锁之后就不会出现文字输出和换行交叉出现的情况了
+std::mutex outLock;
+void echo1() {
+	while (true) {
+		std::lock_guard<mutex> lg(outLock);
+		cout << "echo1" << endl;
 		this_thread::sleep_for(chrono::microseconds(100000));
-		i++;
 	}
 }
 
-void echo2(){
-	int i=0;
-	while(i<10){
-		cout<<"echo2"<<endl;
+void echo2() {
+	while (true) {
+		std::lock_guard<mutex> lg(outLock);
+		cout << "echo2" << endl;
 		this_thread::sleep_for(chrono::microseconds(100000));
-		i++;
 	}
 }
 
-int main(int argc, char* argv[]){
-	auto result1=async(echo1);
+int main(int argc, char* argv[]) {
+	auto result1 = async(launch::async, echo1);
+	auto result2 = async(launch::async, echo2);
 
-	//auto result2=async(echo2);
-	echo2();
+	try {
+		result1.get();
+		result2.get();
+	} catch (exception& e) {
 
-	result1.get();
-	//result2.get();
-    return 0;
+	}
+
+	return 0;
 }
