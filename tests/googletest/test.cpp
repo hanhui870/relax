6f6286cfbc5be8ccf033bf39c60d3128dacec9e7
HanHui <zhujingfa@gmail.com>
2014-09-13 11:20:17 +0800

    gtes已经支持c++11，但是不能存在__STRICT_ANSI__宏变量，cygwin取消掉即可；其他环境未测试

--- tests/googletest/test.cpp
+++ tests/googletest/test.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+
 #include <gtest/gtest.h>
 #include "algorithm.h"
 
@@ -7,4 +8,11 @@ TEST(algorithm, add_multiply)
 {
     EXPECT_EQ(2, add(5, -3));
     EXPECT_EQ(12, multiply(3, 4));
+}
+
+TEST(system, strict_ansi)
+{
+#ifndef __STRICT_ANSI__
+ std::cout<<"__STRICT_ANSI__ macro defined."<<std::endl;
+#endif
 }
--- tests/googletest/test.make
+++ tests/googletest/test.make
@@ -1,6 +1,10 @@
 CPP=g++
-#gtest暂时不支持-std=c++11 flag
-CXXFLAGS=-g
+
+#gtes已经支持c++11，但是不能存在__STRICT_ANSI__宏变量，cygwin取消掉即可；其他环境未测试
+CXXFLAGS=-g -std=c++11 -DGTEST_LANG_CXX11=1 -U__STRICT_ANSI__
+#C语言版定义
+CFLAGS=-g
+
 LIBS=-L/usr/local/lib   -lgtest -lgtest_main
 LIBOBJECTS=algorithm.o test.o
 BINARY=test.exe
