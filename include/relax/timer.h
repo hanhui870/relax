7ed0c9e358006a99e222790865dae4615b3ddaa8
HanHui <zhujingfa@gmail.com>
2014-09-27 16:58:35 +0800

    运行库时间检测使用说明

--- include/relax/timer.h
+++ include/relax/timer.h
@@ -1,3 +1,19 @@
+/**
+ * Relax library
+ *
+ * boost 运行时间检测库
+ *
+ * #include <relax/timer.h>
+ * relax::timer t;
+ *
+ * link dependency:
+ * -lboost_timer.dll -lboost_system.dll
+ *
+ * @author 祝景法
+ * @email zhujingfa@gmail.com
+ *
+ * @since 2014/09/17
+ */
 #include <boost/timer/timer.hpp>
 
 #ifndef RELAX_TIMER_H_
