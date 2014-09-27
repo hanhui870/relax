7fd04b6f4196f71106b1d484824d51eae2031621
HanHui <zhujingfa@gmail.com>
2014-09-27 22:50:40 +0800

    Status方法 添加描述。

--- src/relax/relax.cpp
+++ src/relax/relax.cpp
@@ -10,9 +10,12 @@
 
 namespace relax {
 
+//Status 转移复制构造操作符
 Status::Status(Status&& rvalue) : code_(rvalue.code_), message_(std::move(rvalue.message_)){}
+//Status 普通复制构造操作符
 Status::Status(Status& rvalue) : code_(rvalue.code_), message_(rvalue.message_){}
 
+//Status 转移赋值操作符
 Status& Status::operator=(Status&& rvalue){
     if(this != &rvalue){
         code_=rvalue.code_;
@@ -21,7 +24,7 @@ Status& Status::operator=(Status&& rvalue){
 
     return *this;
 }
-
+//Status 普通赋值操作符
 Status& Status::operator=(Status& rvalue){
     if(this != &rvalue){
         code_=rvalue.code_;
