6225dced41020da733ccd1197cf65196e1f57f93
HanHui <zhujingfa@gmail.com>
2014-11-24 22:47:19 +0800

    ENV global set/get/delete/clear

--- include/relax/env_helper.h
+++ include/relax/env_helper.h
@@ -22,6 +22,11 @@ using std::string;
 
 class EnvHelper{
 public:
+    enum OverWrite {
+        NO=0,
+        YES=1
+    };
+
     /**
      * 获取系统环境变量值
      *
@@ -31,8 +36,22 @@ public:
 
     /**
      * 新设置或覆盖一个变量
+     *
+     * 注: 仅修改程序获取的缓存值
      */
     static Status SetVariable(string key, string value);
+
+    /**
+     * 获取系统全局环境变量值
+     */
+    static Status GetGlobal(string key, string& value);
+
+    /**
+     * 新设置或覆盖一个全局变量
+     *
+     * 注: 会对本进程的所有环境变量产生影响
+     */
+    static Status SetGlobal(string key, string value, OverWrite overwrite=YES);
 };
 
 
